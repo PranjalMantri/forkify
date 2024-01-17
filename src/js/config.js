f6c8e06ee5be28c94bc64e117a81026ccff9bb5f
PranjalMantri <pranjalmantri@gmail.com>
2024-01-18 00:17:20 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e21b54f2-1b1e-4c2c-bdc4-69cc8be6e1ff';
+export const MODAL_CLOSE_SEC = 2.5;
